c314fc9a45fd1f3dbeaf91252a6d3c2c83752896
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-07-25 11:52:54 +0600

    correction

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -17,11 +17,31 @@ const Navbar = () => {
       </div>
 
       <ul className={`navbar-nav ${isOpen ? "show" : ""}`}>
-        <li><Link to="/fix-and-flip" className="nav-link">Fix and Flip</Link></li>
-        <li><Link to="/rental-loans" className="nav-link">Rental Loans</Link></li>
-        <li><Link to="/where-we-lend" className="nav-link">Where We Lend</Link></li>
-        <li><Link to="/about-us" className="nav-link">About Us</Link></li>
-        <li><Link to="/resources" className="nav-link">Resources</Link></li>
+        <li>
+          <Link to="/fix-and-flip" className="nav-link">
+            Fix and Flip
+          </Link>
+        </li>
+        <li>
+          <Link to="/rental-loans" className="nav-link">
+            Rental Loans
+          </Link>
+        </li>
+        <li>
+          <Link to="/where-we-lend" className="nav-link">
+            Where We Lend
+          </Link>
+        </li>
+        <li>
+          <Link to="/about-us" className="nav-link">
+            About Us
+          </Link>
+        </li>
+        <li>
+          <Link to="/resources" className="nav-link">
+            Resources
+          </Link>
+        </li>
         <li>
           <Link to="/get-approved" className="approved-button">
             Get Approved Online
@@ -30,7 +50,11 @@ const Navbar = () => {
       </ul>
 
       <div className="menu-toggle" onClick={toggleMenu}>
-        {isOpen ? <span className="close-icon">&times;</span> : <span>&#9776;</span>}
+        {isOpen ? (
+          <span className="close-icon">&times;</span>
+        ) : (
+          <span>&#9776;</span>
+        )}
       </div>
     </nav>
   );
