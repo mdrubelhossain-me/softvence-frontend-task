8014ee2ac2753b37c769b495512e86252917a64f
mdrubelhossain-me <mdrubelhossain.cse@gmail.com>
2025-07-25 12:17:46 +0600

    navbar complete

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,60 +1,52 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import "./Navbar.css";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">
+        <Link to="/" onClick={() => setIsOpen(false)}>
           <span className="ridge">Ridge</span>
           <span className="street">Street</span>
         </Link>
       </div>
 
       <ul className={`navbar-nav ${isOpen ? "show" : ""}`}>
+        {[
+          { to: "/fix-and-flip", label: "Fix and Flip" },
+          { to: "/rental-loans", label: "Rental Loans" },
+          { to: "/where-we-lend", label: "Where We Lend" },
+          { to: "/about-us", label: "About Us" },
+          { to: "/resources", label: "Resources" },
+        ].map((item) => (
+          <li key={item.to}>
+            <Link
+              to={item.to}
+              className="nav-link"
+              onClick={() => setIsOpen(false)}
+            >
+              {item.label}
+            </Link>
+          </li>
+        ))}
         <li>
-          <Link to="/fix-and-flip" className="nav-link">
-            Fix and Flip
-          </Link>
-        </li>
-        <li>
-          <Link to="/rental-loans" className="nav-link">
-            Rental Loans
-          </Link>
-        </li>
-        <li>
-          <Link to="/where-we-lend" className="nav-link">
-            Where We Lend
-          </Link>
-        </li>
-        <li>
-          <Link to="/about-us" className="nav-link">
-            About Us
-          </Link>
-        </li>
-        <li>
-          <Link to="/resources" className="nav-link">
-            Resources
-          </Link>
-        </li>
-        <li>
-          <Link to="/get-approved" className="approved-button">
+          <Link
+            to="/get-approved"
+            className="approved-button"
+            onClick={() => setIsOpen(false)}
+          >
             Get Approved Online
           </Link>
         </li>
       </ul>
 
       <div className="menu-toggle" onClick={toggleMenu}>
-        {isOpen ? (
-          <span className="close-icon">&times;</span>
-        ) : (
-          <span>&#9776;</span>
-        )}
+        {isOpen ? <FaTimes /> : <FaBars />}
       </div>
     </nav>
   );
